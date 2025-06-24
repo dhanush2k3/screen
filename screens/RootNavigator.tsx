c504f2e33f82695e01c0c8c2c3446473502f14dc
DHANUSH G <122260688+dhanush2k3@users.noreply.github.com>
2025-06-24 17:23:29 +0530

    Update RootNavigator.tsx

--- screens/RootNavigator.tsx
+++ screens/RootNavigator.tsx
@@ -2,10 +2,14 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
 import HomeScreen from './HomeScreen';
 import StackNavigationDemo from '../component/stack/StackNavigationDemo';
+import TabNavigationDemo from '../component/tab/TabNavigationDemo';
+import DrawerNavigationdemo from '../component/Drawer/DrawerNavigationDemo';
 
 export type RootStackParamsList = {
   Home: undefined;
   StackDemo: undefined;
+  TabDemo: undefined;
+  DrawerDemo: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamsList>();
@@ -15,8 +19,11 @@ const RootNavigator: React.FC = () => {
     <Stack.Navigator>
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="StackDemo" component={StackNavigationDemo} />
+      <Stack.Screen name="TabDemo" component={TabNavigationDemo} />
+      <Stack.Screen name="DrawerDemo" component={DrawerNavigationdemo} />
     </Stack.Navigator>
   );
 };
 
 export default RootNavigator;
+
